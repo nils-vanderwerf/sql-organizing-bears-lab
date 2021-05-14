4b0cb935938417ea7cb900632daa8cd1e04fd2eb
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-05-14 21:59:41 +1000

    Bears lab completed

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 15, 'M', 'white', 'angry', 1),
+("Rowdy", 25, 'F', 'black', 'calm', 1),
+("Tabitha", 35, 'F', 'black', 'confused', 1),
+("Sergeant Brown", 45, 'M', 'brown', 'angry', 0),
+("Melissa Brown", 33, 'F', 'brown', 'angry', 0),
+("Grinch", 49, 'M', 'green', 'hates Christmas', 1),
+("Wendy", 104, 'F', 'yellow', 'cheerful', 0),
+(null, 99, 'M', 'black', 'soul-searching', 0);
+
